19c0567265c61964910efcfe182e70ecd9dc2c7e
Vinnicius Correia <78707622+vincorreia@users.noreply.github.com>
2022-12-04 20:05:06 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+///  <reference types="vitest" />
+///  <reference types="vite/client" />
+///  <reference types="vite-plugin-svgr/client" />
+
+import { defineConfig } from 'vite'
+import path from 'path'
+import { readdirSync } from 'fs'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+const absolutePathAliases: { [key: string]: string } = {}
+const srcPath = path.resolve('./src/')
+
+const srcRootContent = readdirSync(srcPath, { withFileTypes: true }).map(
+	dirent => dirent.name.replace(/(\.ts){1}(x?)/, '')
+)
+
+srcRootContent.forEach(directory => {
+	absolutePathAliases[directory] = path.join(srcPath, directory)
+})
+
+export default defineConfig({
+	plugins: [svgr(), react()],
+	test: {
+		globals: true,
+		environment: 'jsdom'
+	},
+	resolve: {
+		alias: {
+			...absolutePathAliases,
+			'@constants': '/src/shared/constants',
+			components: '/src/shared/components/'
+		}
+	}
+})
